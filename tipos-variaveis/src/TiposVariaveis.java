5b14632b3852539b4c25065fe7c9f56ba8d890cf
João <victorjhon779@gmail.com>
2023-09-02 01:23:24 -0300

    commit

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -2,11 +2,15 @@ public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
        //Olhá só o que sei fazer agora, tipos de variáveis. 😁
         
-       String meuNome = "João Victor";
-       int idade = 20;
+       String meuNome = "João Victor ";//texto.
+       int idade = 20; //números inteiros.
+       double salario = 1329.35;//números decimais.
+       boolean verdadeOuMentira =true;//verdadeiro ou falso.
+       char sexo='M';//variável utilizada para caracteristica, USANDO UMA UNICA LETRA.
+       
 
-        System.out.println(idade);
-        System.out.println(meuNome);
+
+    
         
         
     }
